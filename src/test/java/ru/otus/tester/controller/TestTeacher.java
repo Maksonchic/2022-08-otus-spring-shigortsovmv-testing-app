6831dd05158e56a614e6d55b50ff199dbdcb8b5c
Maksim Shchigortsov <makson15681@gmail.com>
2022-10-12 23:12:06 +0300

    repair tests, finally! I understand!

--- src/test/java/ru/otus/tester/controller/TestTeacher.java
+++ src/test/java/ru/otus/tester/controller/TestTeacher.java
@@ -2,17 +2,23 @@ package ru.otus.tester.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.util.ReflectionTestUtils;
+import ru.otus.tester.domain.Question;
 import ru.otus.tester.exceptions.WorkQuestionsException;
 import ru.otus.tester.io.TeacherAskerService;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -24,32 +30,46 @@ public class TestTeacher {
     @MockBean
     private QuestionsHandlerService questionsHandler;
 
+    @SuppressWarnings("unused")
     @Autowired
     private Teacher teacher;
 
-    @Test
-    @DisplayName("Не должно быть исключений, все ответы студента - 2")
-    void notThrowsWorkExc() {
-        Mockito
-                .when(teacherAsker.askQuestion(any()))
-                .thenReturn("2");
-
-        assertDoesNotThrow(teacher::letsTesting);
-
-        // TODO think about
-        Mockito.verify(teacherAsker, Mockito.times(1)).askFirstName();
-        Mockito.verify(teacherAsker, Mockito.times(5)).askQuestion(any());
-        Mockito.verify(questionsHandler, Mockito.times(0)).hasNext();
+    private Question getQuestionPlug() {
+        return new Question(1, "", "1", "");
     }
 
     @Test
     @DisplayName("Сваливаемся на ответе null")
     void throwsWorkExc() {
-        Mockito
-                .when(teacherAsker.askQuestion(any()))
-                .thenReturn(null);
+        when(teacherAsker.askQuestion(any())).thenReturn(null);
+        when(questionsHandler.hasNext()).thenReturn(true);
+        when(questionsHandler.getNext()).thenReturn(getQuestionPlug());
 
         assertThrows(WorkQuestionsException.class, teacher::letsTesting);
+    }
+
+    @Test
+    @DisplayName("Не должно быть исключений, все ответы студента - 2")
+    void notThrowsWorkExc() {
+        when(teacherAsker.askQuestion(any())).thenReturn("2");
+        when(questionsHandler.hasNext()).thenReturn(true);
+
+        ReflectionTestUtils.setField(questionsHandler, "tasks", new ArrayList<Question>(){{
+            add(getQuestionPlug());
+            add(getQuestionPlug());
+            add(getQuestionPlug());
+            add(getQuestionPlug());
+            add(getQuestionPlug());
+        }});
+        when(questionsHandler.hasNext()).thenCallRealMethod();
+        when(questionsHandler.getNext()).thenCallRealMethod();
+
+        assertDoesNotThrow(teacher::letsTesting);
+
+        verify(teacherAsker, times(1)).askFirstName();
+        verify(teacherAsker, times(5)).askQuestion(any());
+        verify(questionsHandler, times(5)).getNext();
+        verify(questionsHandler, times(11)).hasNext();
     }
 }
 
